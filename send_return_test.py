2ab36a637d900ee0456d5f851b1eb46700026a88
minhquang1301 <minhquang65400@gmail.com>
2025-05-21 16:37:11 +0700

    Update send_return_test.py

--- send_return_test.py
+++ send_return_test.py
@@ -7,7 +7,7 @@ channel = connection.channel()
 channel.exchange_declare(exchange='label_tasks', exchange_type='direct', durable=True)
 
 message = {
-    "file_path": r"D:\Desktop\Thực tập\A2A và MCP\Build the Label Detection Agent (A2A)\daily\2025-05-20\somsn59chcghg_shippinglabel_1733967437.pdf"  # sửa đúng đường dẫn file PDF thật
+    "file_path": r"D:\Desktop\Thực tập\A2A và MCP\Build the Label Detection Agent (A2A)\daily\2025-05-20\....pdf"  # sửa đúng đường dẫn file PDF thật
 }
 
 channel.basic_publish(
